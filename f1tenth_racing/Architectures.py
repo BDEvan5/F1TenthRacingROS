6c3ad28cab6f92c89339b3ec0327275bab8f7303
Benjamin <benjaminevans316@gmail.com>
2023-05-18 12:18:52 +0200

    All agents working

--- f1tenth_racing/Architectures.py
+++ f1tenth_racing/Architectures.py
@@ -7,9 +7,11 @@ from matplotlib import pyplot as plt
 NUM_BEAMS = 20
 MAX_SPEED = 8
 MAX_STEER = 0.4
-N_WAYPOINTS = 10
+N_WAYPOINTS_10 = 10
 RANGE_FINDER_SCALE = 10
 WAYPOINT_SCALE = 2.5
+N_WAYPOINTS_20 = 20
+
 
 
 class EndArchitecture:
@@ -53,7 +55,7 @@ class EndArchitecture:
 class PlanningArchitecture:
     def __init__(self, map_name):
         self.waypoint_scale = 2.5
-        self.state_space = N_WAYPOINTS * 2 + 3 + NUM_BEAMS
+        self.state_space = N_WAYPOINTS_10 * 2 + 3 + NUM_BEAMS
         self.action_space = 2
 
         self.track = TrackLine(map_name, False)
@@ -62,10 +64,10 @@ class PlanningArchitecture:
         pose = obs['state'][0:2]
         idx, dists = self.track.get_trackline_segment(pose)
         
-        upcomings_inds = np.arange(idx, idx+N_WAYPOINTS)
-        if idx + N_WAYPOINTS >= self.track.N:
-            n_start_pts = idx + N_WAYPOINTS - self.track.N
-            upcomings_inds[N_WAYPOINTS - n_start_pts:] = np.arange(0, n_start_pts)
+        upcomings_inds = np.arange(idx, idx+N_WAYPOINTS_10)
+        if idx + N_WAYPOINTS_10 >= self.track.N:
+            n_start_pts = idx + N_WAYPOINTS_10 - self.track.N
+            upcomings_inds[N_WAYPOINTS_10 - n_start_pts:] = np.arange(0, n_start_pts)
             
         upcoming_pts = self.track.wpts[upcomings_inds]
         
@@ -76,7 +78,7 @@ class PlanningArchitecture:
         speed = obs['state'][3] / MAX_SPEED
         # anglular_vel = obs['ang_vels_z'][0] / np.pi
         anglular_vel = 0 #! problem...
-        steering_angle = obs['state'][3] / MAX_STEER
+        steering_angle = obs['state'][4] / MAX_STEER
         
         scan = np.array(obs['scan'])
         inds = np.linspace(0, len(scan)-1, NUM_BEAMS).astype(int)
@@ -98,7 +100,6 @@ class PlanningArchitecture:
 
         return action
 
-N_WAYPOINTS = 20
 class TrajectoryArchitecture:
     def __init__(self, map_name):
         self.state_space = N_WAYPOINTS * 3 + 3
@@ -116,10 +117,10 @@ class TrajectoryArchitecture:
         anglular_vel = 0 #! problem here....
         steering_angle = obs['state'][4] / MAX_STEER
         
-        upcomings_inds = np.arange(idx+1, idx+N_WAYPOINTS+1)
-        if idx + N_WAYPOINTS + 1 >= self.track.N:
-            n_start_pts = idx + N_WAYPOINTS + 1 - self.track.N
-            upcomings_inds[N_WAYPOINTS - n_start_pts:] = np.arange(0, n_start_pts)
+        upcomings_inds = np.arange(idx+1, idx+N_WAYPOINTS_20+1)
+        if idx + N_WAYPOINTS_20 + 1 >= self.track.N:
+            n_start_pts = idx + N_WAYPOINTS_20 + 1 - self.track.N
+            upcomings_inds[N_WAYPOINTS_20 - n_start_pts:] = np.arange(0, n_start_pts)
             
         upcoming_pts = self.track.wpts[upcomings_inds]
         
